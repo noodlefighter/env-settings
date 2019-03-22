04875cc04213981ea5d7555f080e4c44820bddae
noodlefighter <noodlefighter@gmail.com>
2019-03-23 01:33:21 +0800

    Added      01_choco-install.ps1 Added      02_software.ps1 Added      Readme.md

---
+++ 02_software.ps1
@@ -0,0 +1,50 @@
+choco upgrade chocolatey
+
+# normal
+cinst -y googlechrome
+cinst -y potplayer
+cinst -y vmware-workstation-player
+cinst -y msys2
+cinst -y steam
+
+# editor
+cinst -y vscode
+cinst -y notepadplusplus.install
+cinst -y typora
+
+# runtimes
+cinst -y javaruntime
+cinst -y dotnet3.5
+cinst -y golang
+
+# lua
+cinst -y lua
+cinst -y zerobrane-studio
+
+# py
+cinst -y python2
+cinst -y python3
+cinst -y pip
+
+# light tools
+cinst -y curl
+cinst -y wget
+cinst -y openssl.light
+cinst -y clink
+cinst -y wireshark
+cinst -y pandoc
+cinst -y mobaxterm
+cinst -y ffmpeg
+cinst -y teamviewer
+cinst -y autohotkey
+
+# de
+cinst -y git
+cinst -y tortoisegit
+cinst -y cmake
+cinst -y etcher
+cinst -y everything
+
+# dev-web
+cinst -y nodejs
+
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Env
+
+```
+set-ExecutionPolicy RemoteSigned
+```
